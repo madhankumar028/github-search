4bab521c06c2b5180bba1681f6a2fc1dda5c1877
madhankumar028 <madhankumar028@gmail.com>
2017-03-15 16:03:09 +0530

    constructed the dom partially for default user

--- autocomplete.js
+++ autocomplete.js
@@ -9,7 +9,6 @@
     const baseUrl = 'https://api.github.com/users';
     
     input.addEventListener('focus', getUsers);
-
     input.addEventListener('keyup', watchChanges);
     
     function watchChanges(event) {
@@ -46,6 +45,8 @@
         var xhr = new XMLHttpRequest(),
             url = `${baseUrl}/${userName}`;
 
+        // showLoading();
+
         xhr.onreadystatechange = function() {
             if (xhr.readyState == XMLHttpRequest.DONE) {
                 constructDefaultUser(JSON.parse(xhr.responseText));
@@ -58,25 +59,56 @@
 
     function constructDefaultUser(user) {
 
-        var autoMenu = document.getElementById('Autocomplete-menu'),
-            userMenu = document.createElement('div'),
-            img = document.createElement('img');
+        var autoMenu        = document.getElementById('Autocomplete-menu'),
+            dataset         = document.createElement('div'),
+            userMenu        = document.createElement('div'),
+            profileCard     = document.createElement('div'),
+            profileDetails  = document.createElement('div'),
+            profileStatus   = document.createElement('div'),
+            img             = document.createElement('img'),
+            name            = document.createElement('div'),
+            mail            = document.createElement('div'),
+            bio             = document.createElement('div'),
+            repo            = document.createElement('div'),
+            followers       = document.createElement('div'),
+            following       = document.createElement('div'),
+            userName        = document.createTextNode(user.login),
+            info            = document.createTextNode(user.bio),
+            repo            = document.createTextNode(user.public_repos),
+            following       = document.createTextNode(user.following),
+            followers       = document.createTextNode(user.followers);
 
         memoize[user.login] = user;
 
-        console.log(memoize);
-
-        userMenu.className = 'pull-left';
+        dataset.className       = 'data-set';
+        userMenu.className      = 'user';
+        profileCard.className   = 'profile-card';
+        profileStatus.className = 'profile-status';
+        name.className          = 'user-name';
+        mail.className          = 'mail';
+        bio.className           = 'bio';
+        repo.className          = 'repo';
+        following.className     = 'following';
+        followers.className     = 'followers';
 
         img.setAttribute('src', user.avatar_url);
-
         img.style.width = '30px';
 
-        userMenu.appendChild(img);
+        profileDetails.appendChild(profileCard);
+        profileDetails.appendChild(userName);
+        profileDetails.appendChild(info);
 
-        autoMenu.appendChild(userMenu);
+        profileStatus.appendChild(repo);
+        profileStatus.appendChild(following);
+        profileStatus.appendChild(followers);        
 
-        console.log(autoMenu);    
+        profileCard.appendChild(img);
+        profileCard.appendChild(profileDetails);
+        profileCard.appendChild(profileStatus);
+
+        console.log(profileCard);
+
+        // hideLoading();
     }
 
     function constructUserInfo(user) {
@@ -84,13 +116,21 @@
         var menu = document.getElementById('menu'),
             p = document.createElement('LI');
         
-        if (user.login !== undefined) {            
+        if (user.login !== undefined) {     
             var text = document.createTextNode(user.login),
-                para = menu.appendChild(text);      
+                para = menu.appendChild(text);
         } else {
             var text = document.createTextNode('user not found'),
                 para = menu.appendChild(text);        
         }
     }
+
+    /*function showLoading() {
+        var loader = document.getElementById('loader').show();
+    }
+
+    function hideLoading() {
+        var loader = document.getElementById('loader').hide();
+    }*/
 
 }());
