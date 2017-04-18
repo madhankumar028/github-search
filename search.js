79548bb2464b7da3fc52403e2289ec5e4687db9d
madhankumar028 <madhankumar028@gmail.com>
2017-04-18 22:11:46 +0530

    externalized user construction styles into main.css

--- search.js
+++ search.js
@@ -92,7 +92,7 @@
                     isUser = user.message || user;                    
 
                 if (isUser !== 'Not Found')                   
-                    constructUser(user, 'keyup');
+                    constructUser(user);
             }
         }
 
@@ -152,7 +152,7 @@
                     isUser = user.message || user;                
 
                 if (isUser !== 'Not Found')                        
-                    constructUser(user, 'onfocus');                
+                    constructUser(user);                
             }
         }
 
@@ -167,9 +167,8 @@
      * @pirvate
      * 
      * @param  {Object} user
-     * @return {[type]}
      */
-    function constructUser(user, eventName) {
+    function constructUser(user) {
         
         var dataset         = document.createElement('div'),
             userMenu        = document.createElement('div'),
@@ -191,8 +190,17 @@
             followers       = document.createTextNode(`Followers: ${user.followers}`),
             info, userName;
         
-        img.className = 'img-circle';        
-        
+        img.className = 'img-circle';
+        profileCard.className = 'profile-card';
+        dataset.className = 'dataset';
+        userMenu.className = 'user-menu';
+        name.className = 'name';
+        bio.className = 'bio';
+        repoSpan.className = 'repo';
+        followersSpan.className = 'followers';
+        followingSpan.className = 'following';
+        profileDetails.className = 'profile-details';
+
         if (user.bio == null) {
             info = document.createTextNode(`${user.login} has not descried anything about him.`);
         } else {
@@ -206,44 +214,6 @@
         }
         
         img.setAttribute('src', user.avatar_url);
-
-        autoMenu.style.boxSizing = 'border-box';
-                
-        img.style.width = '50px';
-        img.style.position = 'relative';
-        img.style.right = '250px';
-        img.style.margin = '20px 0px 0px 0px';
-        
-        dataset.style.margin    = '0px';
-        dataset.style.padding   = '0px 400px';
-
-        userMenu.style.marginRight = '50px';
-
-        name.style.fontWeight = 'bold';
-        name.style.fontSize = '14px';
-
-        bio.style.fontSize = '16px'
-
-        repoSpan.style.marginRight  = '10px';
-        repoSpan.style.color        = '#8899a6';
-        repoSpan.style.fontWeight   = '500';        
-
-        followingSpan.style.marginRight = '10px';
-        followingSpan.style.color       = '#8899a6';        
-        followingSpan.style.fontWeight  = '500';
-
-        followersSpan.style.fontWeight   = '10px';        
-        followersSpan.style.color        = '#8899a6';        
-        followersSpan.style.fontWeight   = '500';
-
-        profileCard.style.paddingLeft = '50px';
-        profileCard.style.width = '500px';
-        profileCard.style.position = 'relative';
-        profileCard.style.left = '-75px';
-        profileCard.style.boxSizing = 'border-box';
-
-        profileDetails.style.position = 'relative';
-        profileDetails.style.margin = '-55px 0px 0px 0px';
 
         name.appendChild(userName);
         bio.appendChild(info);
